596c55cfcb2c8852cd9d0b4f49a277baa87be3e1
Rasmus Praestholm <cervator@gmail.com>
2015-05-31 20:32:38 -0400

    Update Android facade - splices together the LibGDX Gradle template + the old setup

--- src/com/miloshpetrov/sol2/android/SolAndroid.java
+++ src/com/miloshpetrov/sol2/android/SolAndroid.java
@@ -1,17 +1,16 @@
-
 package com.miloshpetrov.sol2.android;
 
 import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.miloshpetrov.sol2.SolAppListener;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.miloshpetrov.sol2.SolApplication;
 
 public class SolAndroid extends AndroidApplication {
-  /**
-   * Called when the activity is first created.
-   */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    initialize(new SolAppListener(), false);
-  }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new SolApplication(), config);
+    }
 }
