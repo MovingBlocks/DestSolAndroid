8a1dc02060593fc3bae12418b080165948bcfc19
Benjamin Amos <benjamin.amos11@gmail.com>
2019-07-10 20:35:54 +0100

    Fixed the Android build.

--- src/com/miloshpetrov/sol2/android/SolAndroid.java
+++ src/com/miloshpetrov/sol2/android/SolAndroid.java
@@ -1,16 +1,32 @@
 package com.miloshpetrov.sol2.android;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import org.destinationsol.SolApplication;
+import org.destinationsol.modules.ModuleManager;
+import org.destinationsol.android.AndroidModuleManager;
 
 public class SolAndroid extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        initialize(new SolApplication(), config);
+
+        ModuleManager manager = new AndroidModuleManager(this);
+
+        try {
+            manager.init();
+        } catch (Exception e) {
+            Log.e("DESTINATION_SOL_INIT", "Failed to initialise ModuleManager.");
+        }
+
+        try {
+            initialize(new SolApplication(manager, 60.0f), config);
+        } catch (Exception e) {
+            Log.e("DESTINATION_SOL", "FATAL ERROR: Forced abort!", e);
+        }
     }
 }
