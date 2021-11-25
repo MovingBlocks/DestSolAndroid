ce4f2dd7238a506aeabeba6b68276a68d9858a2b
Benjamin Amos <benjamin.amos11@gmail.com>
2021-11-25 20:26:00 +0000

    fix(android): fix the game running on Android

--- src/com/miloshpetrov/sol2/android/SolAndroid.java
+++ src/com/miloshpetrov/sol2/android/SolAndroid.java
@@ -6,23 +6,25 @@ import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.google.common.base.Charsets;
-import com.google.common.io.ByteStreams;
-import org.destinationsol.Const;
+import com.google.common.reflect.Reflection;
 import org.destinationsol.SolApplication;
 import org.destinationsol.modules.FacadeModuleConfig;
 import org.terasology.context.Lifetime;
+import org.terasology.gestalt.android.AndroidAssetsFileSource;
 import org.terasology.gestalt.android.AndroidModuleClassLoader;
+import org.terasology.gestalt.android.AndroidModulePathScanner;
 import org.terasology.gestalt.di.ServiceRegistry;
+import org.terasology.gestalt.di.index.UrlClassIndex;
+import org.terasology.gestalt.module.Module;
 import org.terasology.gestalt.module.ModuleEnvironment;
+import org.terasology.gestalt.module.ModuleFactory;
+import org.terasology.gestalt.module.ModuleMetadataJsonAdapter;
+import org.terasology.gestalt.module.ModulePathScanner;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Collections;
 
 public class SolAndroid extends AndroidApplication {
     @Override
@@ -30,136 +32,32 @@ public class SolAndroid extends AndroidApplication {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 
-        File filesDir = getFilesDir();
-        copyModulesToDataDir(filesDir);
-
         try {
-            initialize(new SolApplication(60.0f, new AndroidServices(filesDir, getCodeCacheDir())), config);
+            initialize(new SolApplication(60.0f, new AndroidServices(getAssets(), getCodeCacheDir())), config);
         } catch (Exception e) {
             Log.e("DESTINATION_SOL", "FATAL ERROR: Forced abort!", e);
         }
     }
 
-    private void clearCachedModules(File modulesDir) {
-        File[] files = modulesDir.listFiles();
-        if (files != null) {
-            for (File file : modulesDir.listFiles()) {
-                clearCachedModules(file);
-            }
-        }
-
-        modulesDir.delete();
-    }
-
-    private void copyModulesToDataDir(File dataDir) {
-        File assetVersionFile = new File(dataDir, "version.txt");
-        String versionString = "";
-        if (assetVersionFile.exists()) {
-            try (FileInputStream inputStream = new FileInputStream(assetVersionFile)) {
-                try (InputStreamReader streamReader = new InputStreamReader(inputStream, Charsets.UTF_8)) {
-                    StringBuilder builder = new StringBuilder();
-                    char[] buffer = new char[32];
-                    while (streamReader.read(buffer) != -1) {
-                        builder.append(buffer);
-                    }
-                    versionString = builder.toString();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        boolean refreshCache = (!versionString.equals(Const.VERSION) || com.miloshpetrov.sol2.android.BuildConfig.DEBUG);
-
-        if (refreshCache) {
-            try (FileOutputStream stream = new FileOutputStream(assetVersionFile)) {
-                try (OutputStreamWriter writer = new OutputStreamWriter(stream)) {
-                    writer.write(Const.VERSION);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            clearCachedModules(new File(dataDir, "modules"));
-        }
-
-        copyModules(dataDir, "modules", refreshCache);
-
-        File musicFolder = new File(dataDir, "music");
-        File soundFolder = new File(dataDir, "sound");
-        if (!musicFolder.exists()) {
-            musicFolder.mkdir();
-        }
-
-        if (!soundFolder.exists()) {
-            soundFolder.mkdir();
-        }
-    }
-
-    private void copyModules(File dataDir, String rootDir, boolean replaceFiles) {
-        AssetManager assets = getAssets();
-        try {
-            String[] filesToCopy = assets.list(rootDir);
-            for (String fileToCopy : filesToCopy) {
-                String filePath = rootDir + "/" + fileToCopy;
-                File file = new File(dataDir + "/" + rootDir, fileToCopy);
-                file.mkdirs();
-                if (assets.list(filePath).length > 0) {
-                    // File is a directory
-                    if (!file.exists()) {
-                        file.mkdirs();
-                    }
-                    copyModules(dataDir, filePath, replaceFiles);
-                } else {
-                    if (file.exists() && replaceFiles) {
-                        // Replace old copies with newer ones
-                        file.delete();
-                    }
-
-                    file.createNewFile();
-
-                    byte[] buffer = new byte[512];
-                    try (InputStream inputStream = assets.open(filePath)) {
-                        try (FileOutputStream outputStream = new FileOutputStream(file)) {
-                            ByteStreams.copy(inputStream, outputStream);
-                        } catch (Exception e) {
-                            Log.e("DESTINATION_SOL", "", e);
-                            e.printStackTrace();
-                        }
-                    } catch (IOException e) {
-                        Log.e("DESTINATION_SOL", "", e);
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } catch (IOException e) {
-            Log.e("DESTINATION_SOL", "", e);
-            e.printStackTrace();
-        }
-    }
-
     private static class AndroidServices extends ServiceRegistry {
-        public AndroidServices(File filesDir, File codeCacheDir) {
-            this.with(FacadeModuleConfig.class).lifetime(Lifetime.Singleton).use(() -> new AndroidModuleConfig(filesDir, codeCacheDir));
+        public AndroidServices(AssetManager assets, File codeCacheDir) {
+            this.with(FacadeModuleConfig.class).lifetime(Lifetime.Singleton).use(() -> new AndroidModuleConfig(assets, codeCacheDir));
+            this.with(ModulePathScanner.class).lifetime(Lifetime.Singleton).use(() -> new AndroidModulePathScanner(assets, codeCacheDir));
         }
     }
 
     private static class AndroidModuleConfig implements FacadeModuleConfig {
-        private final File filesDir;
+        private final AssetManager assets;
         private final File codeCacheDir;
 
-        public AndroidModuleConfig(File filesDir, File codeCacheDir) {
-            this.filesDir = filesDir;
+        public AndroidModuleConfig(AssetManager assets, File codeCacheDir) {
+            this.assets = assets;
             this.codeCacheDir = codeCacheDir;
         }
 
         @Override
         public File getModulesPath() {
-            return new File(filesDir, "modules");
+            return new File("modules");
         }
 
         // Android does not allow changing the system security policy.
@@ -167,6 +65,22 @@ public class SolAndroid extends AndroidApplication {
         @Override
         public boolean useSecurityManager() {
             return false;
+        }
+
+        @Override
+        public Module createEngineModule() {
+            try {
+                InputStream engineModuleMetadataStream = assets.open("engine/module.json");
+                return new Module(new ModuleMetadataJsonAdapter().read(new InputStreamReader(engineModuleMetadataStream)),
+                        new AndroidAssetsFileSource(assets, "engine"),
+                        Collections.emptyList(), UrlClassIndex.byClassLoaderPrefix("org.destinationsol"), x -> {
+                    String classPackageName = Reflection.getPackageName(x);
+                    return "org.destinationsol".equals(classPackageName) || classPackageName.startsWith("org.destinationsol.");
+                });
+            } catch (Exception e) {
+                Log.e("DestinationSol", "Error loading engine module!");
+                return null;
+            }
         }
 
         @Override
